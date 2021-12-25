8ae41e83f559ed140063fc3867d7443ace530f87
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-12-25 18:57:36 +0200

    initial commit

---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+   public class Box<T>
+   {
+       private List<T> list = new List<T>();
+
+       public int Count { get => list.Count; }
+
+       public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return element;
+        }
+
+    }
+}
---
+++ Generics - Lab/BoxOfT/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace BoxOfT
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            
+        }
+    }
+}
